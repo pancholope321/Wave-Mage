a35ca26d352645a9a49664422fc2fea1c38120dd
CE-0 <93526532+CE-0@users.noreply.github.com>
2025-11-21 23:12:55 +0000

    Health upgrade in shop, global price values

---
+++ Scenes/shop_item.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var buyBtn = $Buy 
+@export var itemName : String
+@export var level : int 
+@export var isUpgrade : bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void: 
+	level = Global.attrLvlDict[itemName]
+	buyBtn.text = Global.upgradePriceDict[itemName][level]
+
+func updatePrice():
+	Global.attrLvlDict[itemName] += 1
+	level = Global.attrLvlDict[itemName] 
+	buyBtn.text = Global.upgradePriceDict[itemName][level]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
