4e3293b39125b97585b7c45df20dcaac4a359630
pancholope321 <francisco.karamanos@gmail.com>
2025-11-23 20:50:01 -0300

    spear projectile

--- Scripts/projectile_scritp.gd
+++ Scripts/projectile_scritp.gd
@@ -4,7 +4,7 @@ var end=Vector2(0,0)
 var h=0
 var k=0
 var a=0
-
+@export var rotate_offset_deg=0
 func setup_start_end(start_set,end_set):
 	start=start_set
 	end=end_set
@@ -28,8 +28,12 @@ func launch_projectile(regulator):
 	var x2=end.x
 	var x=x1+regulator*(x2-x1)
 	var y=a*((x-h)**2)+k
+	var xderiv=x
+	var yderiv=a*((x-h))
+	var direction=atan(yderiv)
 	self.global_position=Vector2(x,y)
-
+	self.rotation=direction + PI*rotate_offset_deg/180.0
+	print("rotation: ",rotation)
 
 func end_action():
 	#explotion and then queuefree, for example
