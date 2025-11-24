0cac5002337a3cf7c077d71ee9e76a42c26e6085
CE-0 <93526532+CE-0@users.noreply.github.com>
2025-11-24 01:07:41 +0000

    Moved JSON/file functions

--- Scripts/Game/structure_pos_controller.gd
+++ Scripts/Game/structure_pos_controller.gd
@@ -29,10 +29,10 @@ var enemy_json={
 func _ready() -> void:
 	var file_path = "user://my_file.txt"
 	var structure_path="res://ConfigFiles/structure_information_relation.json"
-	var structureJson=load_json_config(structure_path)
-	if check_file_exists(file_path) and !debug:
+	var structureJson=Global.load_json_config(structure_path)
+	if Global.check_file_exists(file_path) and !debug:
 		print("File exists!")
-		var final_json=load_json_config(file_path)
+		var final_json=Global.load_json_config(file_path)
 		waveController.create_list_of_powers(enemy_json.enemy_count,final_json.unlocked_powers,structureJson.power_player_functions)
 	else:
 		waveController.create_list_of_powers(enemy_json.enemy_count,test_json.unlocked_powers,structureJson.power_player_functions)
@@ -40,41 +40,3 @@ func _ready() -> void:
 		
 		
 	pass
-
-
-func check_file_exists(file_path: String) -> bool:
-	return FileAccess.file_exists(file_path)
-
-
-func load_json_config(file_path: String):
-	if not FileAccess.file_exists(file_path):
-		push_error("JSON file does not exist: " + file_path)
-		return null
-	var file = FileAccess.open(file_path, FileAccess.READ)
-	if not file:
-		push_error("Failed to open JSON file: " + file_path)
-		return null
-	var json_string = file.get_as_text()
-	file.close()
-	var json = JSON.new()
-	var parse_result = json.parse(json_string)
-	if parse_result != OK:
-		push_error("JSON Parse Error: " + json.get_error_message() + " at line " + str(json.get_error_line()))
-		return null
-	return json.data
-
-
-func save_json_config(object, path):
-	var user_dir = OS.get_user_data_dir()
-	var file_path = user_dir + path+ "/buffs.json"
-	# Create a FileAccess object for writing
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
-	if file:
-		# Convert your data to JSON string
-		var json_string = JSON.stringify(object)
-		# Write the JSON string to file
-		file.store_string(json_string)
-		file.close()
-	else:
-		print("Error: Could not save file to path")
-	return
