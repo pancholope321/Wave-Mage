4a13952ace97c828eacb2d09b37d4723f15399be
pancholope321 <francisco.karamanos@gmail.com>
2025-11-29 18:50:10 -0300

    added sprites for health potion and sandwich

--- Scripts/Powers/health_potion.gd
+++ Scripts/Powers/health_potion.gd
@@ -4,6 +4,7 @@ extends Node
 func health_potion(context):
 	print("health_potion")
 	var healing=15
+	Sfx.play("NeutralButtonPress")
 	context.player.health=clamp(context.player.health+healing,0,context.player.max_health)
 	context.player.healing()
 	return 0
